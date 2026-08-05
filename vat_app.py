cde36b8e307641127d373000ed134949944f6d30
54217-rgb <54217@yupparaj.ac.th>
2026-08-05 15:16:32 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,4 +6,4 @@ net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-   st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 12  ม.4/2")
+st.write("นายสิรภพ ฝั้นถาวร เลขที่ 12  ม.4/2")
